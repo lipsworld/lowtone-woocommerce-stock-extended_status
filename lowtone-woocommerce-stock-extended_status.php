6d3f4a81ddba11a601c52a85a2579fd2df030c11
Paul van der Meijs <paul@lowtone.nl>
2013-09-05 15:40:06 +0200

    Added status output.

--- lowtone-woocommerce-stock-extended_status.php
+++ lowtone-woocommerce-stock-extended_status.php
@@ -25,6 +25,8 @@ namespace lowtone\woocommerce\stock\extended_status {
 		wp_enqueue_script("lowtone_woocommerce_stock_extended_status", plugins_url("/assets/scripts/jquery.admin.js", __FILE__), array("jquery"));
 	});
 
+	// Add options to inventory tab
+
 	add_action("woocommerce_product_options_stock", function() {
 
 		global $thepostid; // Set by WooCommerce
@@ -37,6 +39,7 @@ namespace lowtone\woocommerce\stock\extended_status {
 			"id" => optionKey("availability"), 
 			"label" => __("Availability", "lowtone_woocommerce_stock_extended_status"), 
 			"options" => array(
+				"unknown" => __("Unknown", "lowtone_woocommerce_stock_extended_status"),
 				"discontinued" => __("Discontinued", "lowtone_woocommerce_stock_extended_status"),
 				"expected" => __("Expected", "lowtone_woocommerce_stock_extended_status"),
 			),
@@ -49,9 +52,9 @@ namespace lowtone\woocommerce\stock\extended_status {
 		echo '<div class="show_if_expected">';
 
 		woocommerce_wp_text_input(array( 
-			"id" => ($availableFromKey = optionKey("available_from")), 
+			"id" => optionKey("available_from"), 
 			"label" => __("Available from", "lowtone_woocommerce_stock_extended_status"), 
-			"value" => ($availableFrom = get_post_meta($thepostid, $availableFromKey, true)) ? date_i18n("Y-m-d", $availableFrom) : "",
+			"value" => ($availableFrom = availableFrom($thepostid)) ? date_i18n("Y-m-d", $availableFrom) : "",
 			"desc_tip" => true, 
 			"description" => __("Custom status. If a custom status is provided it will be displayed with the product. The custom status text is only intended to provide more detailed information and does not affect the availability of the product", "lowtone_woocommerce_stock_extended_status"), 
 			"type" => "date",
@@ -74,6 +77,54 @@ namespace lowtone\woocommerce\stock\extended_status {
 			update_post_meta($postId, optionKey("available_from"), strtotime($_POST[optionKey("available_from")]));
 
 	}, 10, 2);
+
+	// Output
+	
+	add_filter("woocommerce_stock_html", function() {
+		global $product;
+
+		$availability = $product->get_availability();
+
+		if (!$availability["availability"])
+			return;
+
+		$html = '<div class="stock ' . esc_attr($availability["class"]) . '">';
+
+		switch ($availability["class"]) {
+			case "out-of-stock":
+				switch (availability($product->id)) {
+					case "discontinued":
+						$html .= '<p>' . esc_html(__("Discontinued", "lowtone_woocommerce_stock_extended_status")) . '</p>';
+						break;
+
+					case "expected":
+						if (NULL !== ($availableFrom = availableFrom($product->id)) && ($dateDiff = dateDiff($availableFrom)) < 22) {
+
+							if ($dateDiff > 7) 
+								$html .= '<p>' . esc_html(__("Expected within 2-3 weeks", "lowtone_woocommerce_stock_extended_status")) . '</p>';
+							else 
+								$html .= '<p>' . esc_html(__("Expected within days", "lowtone_woocommerce_stock_extended_status")) . '</p>';
+
+						} else 
+							$html .= '<p>' . esc_html(__("Expected (see description)", "lowtone_woocommerce_stock_extended_status")) . '</p>';
+
+						break;
+
+					default:
+						$html .= '<p>' . esc_html($availability["availability"]) . '</p>';
+				}
+
+				break;
+
+			default:
+				$html .= '<p>' . esc_html($availability["availability"]) . '</p>';
+
+		}
+
+		$html .= '</div>';
+
+		return apply_filters("lowtone_woocommerce_stock_extended_status_output", $html, $availability);
+	}, apply_filters("lowtone_woocommerce_stock_extended_status_output_filter_priority", 10), 2);
 				
 	// Register textdomain
 
@@ -85,6 +136,18 @@ namespace lowtone\woocommerce\stock\extended_status {
 
 	function optionKey($key) {
 		return "_lowtone_woocommerce_stock_extended_status_" . $key;
+	}
+
+	function availability($id) {
+		return get_post_meta($id, optionKey("availability"), true) ?: NULL;
+	}
+
+	function availableFrom($id) {
+		return get_post_meta($id, optionKey("available_from"), true) ?: NULL;
+	}
+
+	function dateDiff($date) {
+		return ($date - mktime(0, 0, 0)) / (60*60*24);
 	}
 
 }
